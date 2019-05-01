6ec1d8080d715ace2895798c3f1f2937ecc70edf
unee0353 <unee0353@naver.com>
2019-05-01 16:06:42 +0900

    complete movie app

--- src/App.js
+++ src/App.js
@@ -10,44 +10,54 @@ import Movie from './Movie';
 
 class App extends Component{
 
-  state = {
-    movies : [
-      {
-        title : '어벤져스:엔드게임',
-        poster :'http://image.cine21.com/resize/cine21/poster/2019/0417/11_38_55__5cb691bfbbfd0[X230,330].jpg'
-      },
-      {
-        title : '뷰티플 마인드',
-        poster : 'http://image.cine21.com/resize/cine21/poster/2019/0401/11_21_21__5ca175a16e3a9[X230,330].jpg'
-      },
-      {
-        title : '어스',
-        poster : 'http://image.cine21.com/resize/cine21/poster/2019/0329/18_08_19__5c9de0836c615[X230,330].jpg'
-      },
-      {
-        title : '선희와 슬기',
-        poster : 'http://image.cine21.com/resize/cine21/poster/2019/0312/10_38_58__5c870db299a3a[X230,330].jpg'
-      }
-    
-    ]
+  state = {}
+
+  /* 
+    async, await
+    fetch로 받아온 데이터와 그걸 처리하는 then들을 더욱 간결하고 명료하게 만들어주는 도구
+    fetch와 then을 사용해서 일일이 모든 동작을 지정해주지 않고, fetch가 끝난 다음 바로 실행시킬 것들을 지정할 수 있게 만드는 tool
+
+    asynce:이전라인의 작업이 끝날때까지 기다리지 않고 실행 될 작업
+
+    await : 해당 기능이 끝나는 것을 기다리고, 바로 실행. asynce가 실행 되고 await 실행 됨.
+
+    fetch(주소).then(a => a.json()).then(b=>b.....).then(...).then(...)
+
+  */
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')//fetch() 자바스크립트로 외부의 url을 가져옴
+    .then(b => b.json())//json으로 바꿈
+    .then(c => c.data.movies)
+    .catch(err => console.log(err));//에러가 났을 때 catch
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    })
   }
 
   //5초 뒤에 새로운 영화가 추가 되기
  componentDidMount(){
-  setTimeout(()=>{
-    this.setState({
-      movies:[
-        ...this.state.movies,
-        {
-          title : '하스스톤',
-          poster : 'http://image.cine21.com/resize/cine21/poster/2019/0404/10_55_18__5ca56406df55d[X230,330].jpg'
-        }
-      ]
-    })
-  },5000);
+   
+   this._getMovies()
+
+    
 } 
 
-
+// _renderMovies =()=>{}  이름을 가진 화살표 함수
+_renderMovies =()=>{
+  const movies = this.state.movies.map((x,index)=>{
+    return <Movie 
+    title={x.title} 
+    poster={x.medium_cover_image} 
+    key={index} 
+    genres={x.genres}
+    synopsis={x.synopsis}/>
+  });
+  return movies;
+}
 
 
   render(){
@@ -55,9 +65,16 @@ class App extends Component{
     return(
       <div className = "App">
         {
-          this.state.movies.map((x,index)=>{
-            return <Movie title={x.title} poster={x.poster} key={index}/>
-          })
+          //this.state.movies가 있을 땐 render
+          //state가 비어있는 상황일 땐 loading... 문구
+          this.state.movies ? this._renderMovies() : 'Loading . . .'
+          /* 
+          if(this.state.movie){
+            this._renderMovies()
+          }else{
+            Loading...
+          }
+          */
         }
       </div>
     );
--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,59 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
+import LinesEllipsis from 'react-lines-ellipsis';
 
+/* 
+class Movie extends Component...
+클래스형 컴포넌트
+장점 : state를 쓸 수 있음
+단점 : 코드가 복잡해짐
+
+function Movie({porops1, props2...})
+함수형 컴포넌트 (dumb component)
+단지 어떤 값을 return 해서 출력해주기만을 위한 컴포넌트
+장점 : 코드가 단순함
+단점 : state가 없음
+*/
+
+
+function Movie({title, poster, genres, synopsis}){
+    return(
+        <div className="card">
+            <MoviePoster poster={poster}/>
+            <h1>{title}</h1>
+            <p>
+            {genres.map((y,index)=><MovieGenres genres={y} key={index}/>)}
+            {/* 
+            <MovieGenres genres={sf} key={0}/>
+            <MovieGenres genres={drama} key={1}/>
+            <MovieGenres genres={comic} key={2}/>
+            <MovieGenres genres={romantic} key={3}/>
+            */}
+            </p>
+            <p>
+                {/* lineEllipsis 외부의 컴포넌트를 연결 */}
+                <LinesEllipsis
+                text={synopsis}
+                maxLine='2'
+                ellipsis='...'
+                trimRight
+                basedOn='letters'
+                />   
+            </p>
+        </div>
+    )
+}
+
+function MovieGenres({genres}){
+    return(
+        <span>{genres}</span>
+    )
+}
+    
+
+
+/* 
 class Movie extends Component{
 
     static propTypes = {
@@ -20,7 +72,27 @@ class Movie extends Component{
     }
 }
 
-class MoviePoster extends Component{
+ */
+
+
+/* 
+component
+- props : 부모로부터 받아오는 값(읽기 전용)
+- state : 각각의 컴포넌트가 가지는 객체
+          state에 변경사항이 있을 때 마다 componenet가 새로 랜더링 된다.
+*/
+
+
+
+function MoviePoster({poster}){
+    return(
+        <img src={poster} alt="Movie Poster"/>
+    )
+}
+
+
+
+/* class MoviePoster extends Component{
     static propTypes = {
         poster : PropTypes.string.isRequired
     }
@@ -28,6 +100,15 @@ class MoviePoster extends Component{
     render(){
         return <img src={this.props.poster}/>;
     }
+} */
+
+Movie.propTypes={
+    title:PropTypes.string.isRequired,
+    poster:PropTypes.string.isRequired
+}
+
+MoviePoster.propTypes={
+    poster:PropTypes.string.isRequired
 }
 
 export default Movie;
